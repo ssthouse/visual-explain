8c9b178341a971ca9a337a108375ed307fafb85d
ssthouse <ssthouse@163.com>
2018-07-04 14:11:43 +0800

    Add more router

---
+++ src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Index Page</div>
+</template>
+
+<script>
+export default {
+  name: 'Root',
+  data () {
+    return {}
+  },
+  mounted () {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Root from '@/components/Root'
+import DOMRender from '@/components/DomRender'
+import Index from '../components/Index'
 
 Vue.use(Router)
 
@@ -8,8 +9,17 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Root',
-      component: Root
+      redirect: '/list'
+    },
+    {
+      path: '/list',
+      name: 'index',
+      component: Index
+    },
+    {
+      path: '/list/domRender',
+      name: 'domRender',
+      component: DOMRender
     }
   ]
 })
