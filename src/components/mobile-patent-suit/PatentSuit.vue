6122a5bd31c16388a733480c401141f598a46727
ssthouse <ssthouse@163.com>
2018-07-13 19:48:34 +0800

    Add old version d3 code

--- src/components/mobile-patent-suit/PatentSuit.vue
+++ src/components/mobile-patent-suit/PatentSuit.vue
@@ -6,7 +6,22 @@
 </template>
 
 <script>
-export default {}
+import GraphGenerator from './graphGenerator'
+
+export default {
+  name: 'PatentSuit',
+  data() {
+    return {
+      graphGenerator: null
+    }
+  },
+  methods: {},
+  mounted() {
+    const svgNode = this.$d3.select('#main-svg')
+    this.graphGenerator = new GraphGenerator(svgNode, this.$d3)
+    this.graphGenerator.draw()
+  }
+}
 </script>
 
 <style lang="less">
@@ -18,6 +33,36 @@ export default {}
   .main-svg {
     width: 60%;
     background-color: #eeeeee;
+
+    .link {
+      fill: none;
+      stroke: #666;
+      stroke-width: 1.5px;
+    }
+
+    #licensing {
+      fill: green;
+    }
+
+    .link.licensing {
+      stroke: green;
+    }
+
+    .link.resolved {
+      stroke-dasharray: 0, 2 1;
+    }
+
+    circle {
+      fill: #ccc;
+      stroke: #333;
+      stroke-width: 1.5px;
+    }
+
+    text {
+      font: 10px sans-serif;
+      pointer-events: none;
+      text-shadow: 0 1px 0 #fff, 1px 0 0 #fff, 0 -1px 0 #fff, -1px 0 0 #fff;
+    }
   }
 
   .explain-panel {
