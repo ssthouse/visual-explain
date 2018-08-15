8ccd3ef455418e52efdfe51d52106738fad14ba2
ssthouse <ssthouse@163.com>
2018-08-15 10:49:33 +0800

    Add CodeMirror to help edit json file(tree structure)

--- src/components/depth-first-traversal/DftExplain.vue
+++ src/components/depth-first-traversal/DftExplain.vue
@@ -1,23 +1,44 @@
 <template>
   <div>
     <tree-editor></tree-editor>
+    <textarea ref="treeJsonCode" v-model="treeJsonCode"></textarea>
   </div>
 </template>
 <script>
 import { constructTestData, Dft } from './dft'
 import TreeEditor from './TreeEditor.vue'
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/mode/javascript/javascript.js'
 
 export default {
   name: 'DftExplain',
   components: { 'tree-editor': TreeEditor },
   data() {
-    return {}
+    return {
+      treeJsonCode: '{value: 1, childNodes : []}',
+      codeMirror: null
+    }
   },
-  methods: {},
+  methods: {
+    testCodeMirror() {
+      this.codeMirror = CodeMirror.fromTextArea(this.$refs.treeJsonCode, {
+        mode: 'javascript'
+      })
+    }
+  },
   mounted() {
     const rootNode = constructTestData()
     const dft = new Dft(rootNode, null)
     dft.start()
+    this.testCodeMirror()
   }
 }
 </script>
+<style lang="less">
+.CodeMirror {
+  text-align: left !important;
+  padding: 16px;
+  font-family: 'Courier New', Courier, monospace;
+}
+</style>
