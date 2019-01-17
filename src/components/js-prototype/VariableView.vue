20f545d53d8dd552412cdd07927aa291ca724547
ssthouse <ssthouse@163.com>
2019-01-17 14:17:48 +0800

    Implement simple prototype popover

--- src/components/js-prototype/VariableView.vue
+++ src/components/js-prototype/VariableView.vue
@@ -3,6 +3,7 @@
     <svg id="variable-view">
       {{variable.toString()}}
     </svg>
+    <div class="tooltip-container"></div>
   </div>
 </template>
 
@@ -67,6 +68,21 @@ export default {
         .on('mouseout', self.tipCaller.hide)
 
       circleSelection.exit().remove()
+
+      this.$d3
+        .select('#variable-view')
+        .selectAll('text')
+        .data(nodeList)
+        .enter()
+        .append('text')
+        .text(d => {
+          return d.proto !== null ? d.proto.constructor.name : 'null'
+        })
+        .attr('x', (data, index) => {
+          return self.circieRadius + index * 100
+        })
+        .attr('y', 100)
+        .attr('dy', '8')
     },
     update() {
       let headProtoNode = this.constructPrototypeNodeList()
@@ -83,17 +99,20 @@ export default {
     function objToJson(obj) {
       if (!obj) return 'null'
       let result = {}
-      Object.keys(obj).forEach(key => {
-        result[key] = obj[key].toString()
+      Object.getOwnPropertyNames(obj).forEach(key => {
+        result[key] = obj[key] + ''
       })
       return JSON.stringify(result, null, 4)
     }
 
     this.tipCaller = tip()
       .attr('class', 'tip-container')
-      .offset([-10, 0])
+      .offset([-20, 0])
       .html(d => {
-        return `<div style="white-space: pre;">${objToJson(d.proto)}</div>`
+        return `<div class="tooltip-container" style="white-space: pre; background-color: white;padding: 8px;
+          border-radius: 8px; box-shadow: 0 2px 4px 0 rgba(0,0,0,0.16),0 2px 10px 0 rgba(0,0,0,0.12);">${objToJson(
+           d.proto
+         )}</div>`
       })
     let svgSelection = this.$d3.select('#variable-view')
     svgSelection.call(this.tipCaller)
@@ -103,6 +122,12 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less">
+.tip-container {
+  background-color: red;
+  border-radius: 8px;
+  font-size: 100px;
+}
+
 .variable-view {
   min-height: 240px;
   width: 100%;
@@ -110,6 +135,19 @@ export default {
   #variable-view {
     width: 100%;
     height: 500px;
+  }
+
+  svg {
+    circle {
+      cursor: pointer;
+    }
+
+    text {
+      font-weight: bold;
+      font-size: 20px;
+      pointer-events: none;
+      text-anchor: middle;
+    }
   }
 }
 </style>
